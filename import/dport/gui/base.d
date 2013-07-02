7e0bd2ea9367cffb9d3c568a80b883200f045dab
deviator <code.viator@gmail.com>
2013-07-03 01:16:18 +0400

    work for sdl2

--- import/dport/gui/base.d
+++ import/dport/gui/base.d
@@ -13,26 +13,61 @@ alias vrect!int irect;
  +/
 struct KeyboardEvent
 {
+    /++ модификаторы (SHIFT, ALT etc) +/
+    enum Mod
+    {
+        NONE = 0x0000,
+        LSHIFT = 0x0001,
+        RSHIFT = 0x0002,
+        LCTRL = 0x0040,
+        RCTRL = 0x0080,
+        LALT = 0x0100,
+        RALT = 0x0200,
+        LGUI = 0x0400,
+        RGUI = 0x0800,
+        NUM = 0x1000,
+        CAPS = 0x2000,
+        MODE = 0x4000,
+        CTRL = (LCTRL|RCTRL),
+        SHIFT = (LSHIFT|RSHIFT),
+        ALT = (LALT|RALT),
+        GUI = (LGUI|RGUI),
+    }
     /++ нажата или отпущена клавиша +/
-    bool pressed; 
+    bool pressed;
+    /++ если нажата и удерживается +/
+    bool repeat;
     /++ код клавиши +/
-    uint code;    
-    /++ символ в unicode +/
-    wchar symbol; 
-    /++ модификаторы (SHIFT, ALT etc) See_Also: SDL key mods +/
-    int mod;      
+    uint scan;
+    uint key;
+    /++ модификатор +/
+    uint mod;
 }
+
+/++
+ событие ввода текста
+ +/
+struct TextEvent { dchar ch; }
 
 /++
  событие мыши
  +/
 struct MouseEvent
 {
-    enum Type { PRESSED, RELEASED, MOTION };
+    enum Type { PRESSED, RELEASED, MOTION, WHEEL };
+    enum Button
+    {
+        LEFT   = 1<<0,
+        MIDDLE = 1<<1,
+        RIGHT  = 1<<2,
+        X1     = 1<<3,
+        X2     = 1<<4,
+    }
     /++ тип события +/
     Type type; 
-    /++ mask for motion, button for pressed/released +/
-    long info; 
+    /++ mask for motion, button for pressed/released, 0 for wheel+/
+    uint btn; 
+    ivec2 scroll = ivec2( 0, 0 );
 }
 
 /++
@@ -62,11 +97,13 @@ struct JoyEvent
 
 alias const ref ivec2 in_ivec2;
 alias const ref KeyboardEvent in_KeyboardEvent;
+alias const ref TextEvent in_TextEvent;
 alias const ref MouseEvent in_MouseEvent;
 alias const ref JoyEvent in_JoyEvent;
 alias const ref irect in_irect;
 
 alias IfListSignal!(in_ivec2, in_KeyboardEvent) IfKeyboardSignal;
+alias IfListSignal!(in_ivec2, in_TextEvent ) IfTextSignal;
 alias IfListSignal!(in_ivec2, in_MouseEvent) IfMouseSignal;
 alias IfListSignal!(in_ivec2, in_JoyEvent) IfJoySignal;
 alias Signal!(in_irect) ReshapeSignal;
@@ -82,6 +119,8 @@ class EventProc
 {
     /++ события клавы с условием ( делегаты принимают положение мыши in ivec2 и само событие ) +/
     IfKeyboardSignal keyboard; 
+    /++ события ввода символов с условием +/
+    IfTextSignal evtext;
     /++ события мыши с условием ( делегаты принимают положение мыши и само событие для мыши ) +/
     IfMouseSignal mouse;       
     /++ события джостика с условием +/
--- import/dport/gui/glsdlapp.d
+++ import/dport/gui/glsdlapp.d
@@ -3,8 +3,11 @@ module dport.gui.glsdlapp;
 import derelict.sdl2.sdl;
 import derelict.opengl3.gl3;
 
-import std.datetime;
-import std.getopt;
+import std.datetime,
+       std.getopt,
+       std.conv,
+       std.utf,
+       std.string;
 
 import dport.gui.base;
 import dport.utils.system;
@@ -15,8 +18,15 @@ string toDString( const(char*) c_str )
 {
     string buf;
     char *ch = cast(char*)c_str;
-    while( *ch != '\0' )
-        buf ~= *(ch++);
+    while( *ch != '\0' ) buf ~= *(ch++);
+    return buf;
+}
+
+string toDStringFix(size_t S)( char[S] c_buf )
+{
+    string buf;
+    foreach( c; c_buf )
+        buf ~= c;
     return buf;
 }
 
@@ -40,7 +50,7 @@ private:
     {
         string joy_name = "";
         bool audioinit = false;
-        bool resizable = true;
+        bool resizable = false;
         string title = "work";
         ivec2 glver = ivec2( 3, 1 );
 
@@ -52,8 +62,6 @@ private:
 
         if( SDL_Init( sdlinitflags ) < 0 )
             throw new GLSDLAppException( "Couldn't init SDL: " ~ toDString(SDL_GetError()) );
-
-        //SDL_EnableUNICODE(1);
 
         int num_joys = SDL_NumJoysticks();
         if( num_joys > 0 && joy_name.length )
@@ -83,8 +91,8 @@ private:
         SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 
         window = SDL_CreateWindow( title.ptr,
-                                   SDL_WINDOWPOS_CENTERED,
-                                   SDL_WINDOWPOS_CENTERED,
+                                   SDL_WINDOWPOS_UNDEFINED,
+                                   SDL_WINDOWPOS_UNDEFINED,
                                    winsize.w, winsize.h,
                                    SDL_WINDOW_OPENGL | 
                                    SDL_WINDOW_SHOWN | 
@@ -129,60 +137,95 @@ private:
         vh.draw();
     }
 
-    void window_eh( ubyte event, long data1, long data2 )
+    void window_eh( in SDL_WindowEvent ev )
     {
-        switch( event ) 
+        switch( ev.event ) 
         {
-        case SDL_WINDOWEVENT_SHOWN: break;
-        case SDL_WINDOWEVENT_HIDDEN: break;
-        case SDL_WINDOWEVENT_EXPOSED: break;
-        case SDL_WINDOWEVENT_MOVED: break;
-        case SDL_WINDOWEVENT_RESIZED:
-            winsize.w = cast(uint)data1;
-            winsize.h = cast(uint)data2;
-            vh.reshape( irect( ivec2(0,0), winsize ) );
-            glViewport( 0, 0, cast(int)data1, cast(int)data2 );
+        case SDL_WINDOWEVENT_NONE: break;
+        case SDL_WINDOWEVENT_SHOWN: 
+            debug log.Debug( format( "window %d shown", ev.windowID ) );
             break;
-        case SDL_WINDOWEVENT_MINIMIZED: break;
-        case SDL_WINDOWEVENT_MAXIMIZED: break;
-        case SDL_WINDOWEVENT_RESTORED: break;
-        case SDL_WINDOWEVENT_ENTER: break;
-        case SDL_WINDOWEVENT_LEAVE: break;
-        case SDL_WINDOWEVENT_FOCUS_GAINED: break;
-        case SDL_WINDOWEVENT_FOCUS_LOST: break;
-        case SDL_WINDOWEVENT_CLOSE: break;
+        case SDL_WINDOWEVENT_HIDDEN: 
+            debug log.Debug( format( "window %d hidden", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_EXPOSED: 
+            debug log.Debug( format( "window %d exposed", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_MOVED: 
+            debug log.Debug( format( "window %d moved", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_RESIZED:
+            debug log.Debug( format( "window %d resized", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_SIZE_CHANGED: 
+            winsize.w = cast(uint)ev.data1;
+            winsize.h = cast(uint)ev.data2;
+            vh.reshape( irect( ivec2(0,0), winsize ) );
+            glViewport( 0, 0, cast(int)ev.data1, cast(int)ev.data2 );
+            debug log.Debug( format( "window %d size changed", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_MINIMIZED:    
+            debug log.Debug( format( "window %d minimized", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_MAXIMIZED:    
+            debug log.Debug( format( "window %d maximized", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_RESTORED:     
+            debug log.Debug( format( "window %d restored", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_ENTER:        
+            vh.activate();
+            debug log.Debug( format( "window %d enter", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_LEAVE:        
+            vh.release();
+            debug log.Debug( format( "window %d leave", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_FOCUS_GAINED: 
+            debug log.Debug( format( "window %d focus gained", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_FOCUS_LOST:   
+            debug log.Debug( format( "window %d focus lost", ev.windowID ) );
+            break;
+        case SDL_WINDOWEVENT_CLOSE:        
+            debug log.Debug( format( "window %d close", ev.windowID ) );
+            break;
         default: break;
         }
     }
 
-    void keyboard_eh( in ivec2 mpos, ubyte state, int scancode, ulong symchar, ushort mod ) 
+    void keyboard_eh( in ivec2 mpos, in SDL_KeyboardEvent ev ) 
     { 
-        /*
-         * SDL mod description
-         *
-         * KMOD_NONE     * 0 (no modifier is applicable)
-         * KMOD_LSHIFT   * the left Shift key is down
-         * KMOD_RSHIFT   * the right Shift key is down
-         * KMOD_LCTRL    * the left Ctrl (Control) key is down
-         * KMOD_RCTRL    * the right Ctrl (Control) key is down
-         * KMOD_LALT     * the left Alt key is down
-         * KMOD_RALT     * the right Alt key is down
-         * KMOD_LGUI     * the left GUI key (often the Windows key) is down
-         * KMOD_RGUI     * the right GUI key (often the Windows key) is down
-         * KMOD_NUM      * the Num Lock key (may be located on an extended keypad) is down
-         * KMOD_CAPS     * the Caps Lock key is down
-         * KMOD_MODE     * the AltGr key is down
-         * KMOD_CTRL     * (KMOD_LCTRL|KMOD_RCTRL)
-         * KMOD_SHIFT    * (KMOD_LSHIFT|KMOD_RSHIFT)
-         * KMOD_ALT      * (KMOD_LALT|KMOD_RALT)
-         * KMOD_GUI      * (KMOD_LGUI|KMOD_RGUI)
-         * KMOD_RESERVED * reserved for future use
-         */
-        vh.keyboard( ivec2( mpos.x, mpos.y ), 
-                KeyboardEvent( state == SDL_PRESSED, 
-                               scancode,
-                               cast(wchar)symchar, 
-                               mod ) );
+        KeyboardEvent oev;
+        oev.pressed = (ev.state == SDL_PRESSED);
+        oev.scan = ev.keysym.scancode;
+        oev.key = ev.keysym.sym;
+        oev.repeat = cast(bool)ev.repeat;
+        
+        oev.mod = 
+                  ( ev.keysym.mod & KMOD_LSHIFT ? KeyboardEvent.Mod.LSHIFT : 0 ) |
+                  ( ev.keysym.mod & KMOD_RSHIFT ? KeyboardEvent.Mod.RSHIFT : 0 ) |
+                  ( ev.keysym.mod & KMOD_LCTRL  ? KeyboardEvent.Mod.LCTRL  : 0 ) |
+                  ( ev.keysym.mod & KMOD_RCTRL  ? KeyboardEvent.Mod.RCTRL  : 0 ) | 
+                  ( ev.keysym.mod & KMOD_LALT   ? KeyboardEvent.Mod.LALT   : 0 ) |
+                  ( ev.keysym.mod & KMOD_RALT   ? KeyboardEvent.Mod.RALT   : 0 ) |
+                  ( ev.keysym.mod & KMOD_LGUI   ? KeyboardEvent.Mod.LGUI   : 0 ) |
+                  ( ev.keysym.mod & KMOD_RGUI   ? KeyboardEvent.Mod.RGUI   : 0 ) |
+                  ( ev.keysym.mod & KMOD_NUM    ? KeyboardEvent.Mod.NUM    : 0 ) |
+                  ( ev.keysym.mod & KMOD_CAPS   ? KeyboardEvent.Mod.CAPS   : 0 ) |
+                  ( ev.keysym.mod & KMOD_MODE   ? KeyboardEvent.Mod.MODE   : 0 ) |
+                  ( ev.keysym.mod & KMOD_CTRL   ? KeyboardEvent.Mod.CTRL   : 0 ) |
+                  ( ev.keysym.mod & KMOD_SHIFT  ? KeyboardEvent.Mod.SHIFT  : 0 ) |
+                  ( ev.keysym.mod & KMOD_ALT    ? KeyboardEvent.Mod.ALT    : 0 ) |
+                  ( ev.keysym.mod & KMOD_GUI    ? KeyboardEvent.Mod.GUI    : 0 );
+
+        vh.keyboard( mpos, oev );
+    }
+
+    void textinput_eh( in ivec2 mpos, in SDL_TextInputEvent ev )
+    {
+        auto str = toUTF32( ev.text[0 .. 4].dup );
+        vh.evtext( mpos, TextEvent( str[0] ) );
     }
 
     void joystick_eh( in ivec2 mpos, uint joy, JoyEvent.Type type, size_t no )
@@ -212,38 +255,42 @@ private:
         vh.joystick( mpos, je );
     }
 
-    void mouse_button_eh( in ivec2 mpos, ubyte state, uint button ) 
+    void mouse_button_eh( in ivec2 mpos, in SDL_MouseButtonEvent ev ) 
     { 
-        /*
-         * mouse buttons
-         *
-         * SDL_BUTTON_LEFT
-         * SDL_BUTTON_MIDDLE
-         * SDL_BUTTON_RIGHT
-         * SDL_BUTTON_X1
-         * SDL_BUTTON_X2
-         */
+        auto me = MouseEvent( ev.state == SDL_PRESSED ? MouseEvent.Type.PRESSED : 
+                                MouseEvent.Type.RELEASED, 0 );
+        switch( ev.button )
+        {
+            case SDL_BUTTON_LEFT:   me.btn = MouseEvent.Button.LEFT; break;
+            case SDL_BUTTON_MIDDLE: me.btn = MouseEvent.Button.MIDDLE; break;
+            case SDL_BUTTON_RIGHT:  me.btn = MouseEvent.Button.RIGHT; break;
+            case SDL_BUTTON_X1:     me.btn = MouseEvent.Button.X1; break;
+            case SDL_BUTTON_X2:     me.btn = MouseEvent.Button.X2; break;
+            default: 
+                throw new GLSDLAppException( "Undefined mouse button: " ~ to!string( ev.button ) );
+                break;
+        }
 
-        vh.mouse( ivec2( mpos.x, mpos.y ),
-                 MouseEvent( 
-                    state == SDL_PRESSED ? MouseEvent.Type.PRESSED : MouseEvent.Type.RELEASED,
-                    button ) );
-
+        vh.mouse( mpos, me );
     }
 
-    void mouse_motion_eh( in ivec2 mpos, ubyte state ) 
+    void mouse_motion_eh( in ivec2 mpos, in SDL_MouseMotionEvent ev ) 
     { 
-        /*
-         * mouse motion states
-         *
-         * SDL_BUTTON_LMASK 
-         * SDL_BUTTON_MMASK
-         * SDL_BUTTON_RMASK
-         * SDL_BUTTON_X1MASK
-         * SDL_BUTTON_X2MASK
-         */
-        vh.mouse( ivec2( mpos.x, mpos.y ), 
-                MouseEvent( MouseEvent.Type.MOTION, state ) );
+        auto me = MouseEvent( MouseEvent.Type.MOTION, 0 );
+        me.btn = 
+          ( ev.state & SDL_BUTTON_LMASK  ? MouseEvent.Button.LEFT : 0 ) |
+          ( ev.state & SDL_BUTTON_MMASK  ? MouseEvent.Button.MIDDLE : 0 ) |
+          ( ev.state & SDL_BUTTON_RMASK  ? MouseEvent.Button.RIGHT : 0 ) |
+          ( ev.state & SDL_BUTTON_X1MASK ? MouseEvent.Button.X1 : 0 ) |
+          ( ev.state & SDL_BUTTON_X2MASK ? MouseEvent.Button.X2 : 0 );
+        vh.mouse( mpos, me );
+    }
+
+    void mouse_wheel_eh( in ivec2 mpos, in SDL_MouseWheelEvent ev )
+    {
+        auto me = MouseEvent( MouseEvent.Type.WHEEL, 0 );
+        me.scroll = ivec2( ev.x, ev.y );
+        vh.mouse( mpos, me );
     }
 
 public:
@@ -270,24 +317,8 @@ public:
 
         glViewport( 0, 0, winsize.w, winsize.h );
         vh.rect = irect( 0, 0, winsize.w, winsize.h );
-        vh.activate();
         debug log.Debug( "set view: ", vh.rect._rect );
     }
-
-    //void setVideoMode( uint w, uint h )
-    //{
-    //    debug log.Debug( "set size: (", w, ", ", h, ")" );
-    //    winsize.w = w;
-    //    winsize.h = h;
-    //    if( SDL_SetVideoMode( w, h, 0, SDL_OPENGL ) == null )
-    //        throw new GLSDLAppException( "Failed to set video mode: " ~ toDString(SDL_GetError()) );
-
-    //    if( vh !is null )
-    //    {
-    //        glViewport( 0, 0, winsize.w, winsize.h );
-    //        vh.rect = irect( 0, 0, w, h );
-    //    }
-    //}
 
     void mainLoop()
     {
@@ -307,18 +338,13 @@ public:
                         run = false; 
                         debug log.Debug( "SDL_QUIT" );
                         break;
-                    case SDL_WINDOWEVENT:
-                        window_eh( event.window.event, 
-                                   event.window.data1, 
-                                   event.window.data2 );
-                        break;
+
+                    case SDL_WINDOWEVENT: window_eh( event.window ); break;
+
+                    case SDL_TEXTINPUT: textinput_eh( mpos, event.text ); break;
+
                     case SDL_KEYDOWN: 
-                    case SDL_KEYUP:
-                        keyboard_eh( mpos, event.key.state,
-                                event.key.keysym.scancode,
-                                event.key.keysym.unicode,
-                                event.key.keysym.mod );
-                        break;
+                    case SDL_KEYUP: keyboard_eh( mpos, event.key ); break;
 
                     case SDL_JOYAXISMOTION: 
                         joystick_eh( mpos, event.jaxis.which, 
@@ -338,24 +364,20 @@ public:
                                 JoyEvent.Type.HAT, event.jhat.hat );
                         break;
 
-                    case SDL_MOUSEBUTTONDOWN: goto case;
+                    case SDL_MOUSEBUTTONDOWN: 
                     case SDL_MOUSEBUTTONUP:
                         mpos.x = event.button.x;
                         mpos.y = event.button.y;
-                        mouse_button_eh( mpos, 
-                                         event.button.state,
-                                         event.button.button );
+                        mouse_button_eh( mpos, event.button );
                         break;
                     case SDL_MOUSEMOTION:
                         mpos.x = event.motion.x;
                         mpos.y = event.motion.y;
-                        mouse_motion_eh( mpos, event.motion.state );
+                        mouse_motion_eh( mpos, event.motion );
                         break;
-                    //case SDL_MOUSEWHEEL:
-                    //    mouse_wheel_eh( intpoint( event.wheel.x, 
-                    //                              event.wheel.y ),
-                    //                    mousepos );
-                    //    break;
+                    case SDL_MOUSEWHEEL:
+                        mouse_wheel_eh( mpos, event.wheel );
+                        break;
                     default:
                         break;
                 }
