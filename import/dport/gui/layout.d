e0a76382ee6d186fbe8951c0999feb8c0a7cf13d
deviator <code.viator@gmail.com>
2013-08-16 18:55:32 +0400

    simple line layout

--- import/dport/gui/layout.d
+++ import/dport/gui/layout.d
@@ -5,10 +5,20 @@ import dport.gui.element;
 import dport.utils.system;
 mixin( defaultModuleLogUtils("LayoutException") );
 
-class LineLayout: Layout
+// TODO: написать больше расскладок
+
+class LineWLayout: Layout
 {
     override void opCall( irect bbox, Element[] chlist )
     {
-        // TODO
+        if( chlist.length == 0 ) return;
+        float sh = bbox.h;
+        auto dh = sh / cast(float)chlist.length;
+        auto bufh = 0;
+        foreach( i, ch; chlist )
+        {
+            ch.reshape( irect( 0, cast(int)bufh, bbox.w, cast(int)dh ) );
+            bufh += dh;
+        }
     }
 }
