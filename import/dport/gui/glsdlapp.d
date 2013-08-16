345755475e9a798d7a97950e9f29e4f13ea9bef3
deviator <code.viator@gmail.com>
2013-08-17 01:30:23 +0400

    glScissor winsize -> vh.rect.size

--- import/dport/gui/glsdlapp.d
+++ import/dport/gui/glsdlapp.d
@@ -134,7 +134,7 @@ private:
 
     void draw()
     {
-        glScissor( 0, 0, winsize.w, winsize.h );
+        glScissor( 0, 0, vh.rect.w, vh.rect.h );
         glClear( GL_COLOR_BUFFER_BIT | 
                  GL_DEPTH_BUFFER_BIT );
         vh.draw();
